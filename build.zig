dc4232057ca5f302ef90a02da9ae6d2a8bebc794
Jakub Konka <kubkon@jakubkonka.com>
2022-11-22 22:27:02 +0100

    example: turn off logs unless explicitly built-in and requested

--- build.zig
+++ build.zig
@@ -3,6 +3,8 @@ const std = @import("std");
 pub fn build(b: *std.build.Builder) void {
     const mode = b.standardReleaseOptions();
 
+    const enable_logging = b.option(bool, "log", "Whether to enable logging") orelse false;
+
     const lib = b.addStaticLibrary("yaml", "src/yaml.zig");
     lib.setBuildMode(mode);
     lib.install();
@@ -10,23 +12,30 @@ pub fn build(b: *std.build.Builder) void {
     var yaml_tests = b.addTest("src/yaml.zig");
     yaml_tests.setBuildMode(mode);
 
-    var e2e_tests = b.addTest("test/test.zig");
-    e2e_tests.setBuildMode(mode);
-    e2e_tests.addPackagePath("yaml", "src/yaml.zig");
-
-    const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&yaml_tests.step);
-    test_step.dependOn(&e2e_tests.step);
-
     const example = b.addExecutable("yaml", "examples/yaml.zig");
     example.setBuildMode(mode);
     example.addPackagePath("yaml", "src/yaml.zig");
-    example.step.dependOn(b.getInstallStep());
 
-    const path_to_yaml = b.option([]const u8, "input-yaml", "Path to input yaml file") orelse "examples/simple.yml";
+    const example_opts = b.addOptions();
+    example.addOptions("build_options", example_opts);
+    example_opts.addOption(bool, "enable_logging", enable_logging);
 
-    const run_example = example.run();
-    run_example.addArg(path_to_yaml);
-    const run_example_step = b.step("run", "Runs examples/yaml.zig");
-    run_example_step.dependOn(&run_example.step);
+    example.install();
+
+    const run_cmd = example.run();
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run example program parser");
+    run_step.dependOn(&run_cmd.step);
+
+    const test_step = b.step("test", "Run library tests");
+    test_step.dependOn(&yaml_tests.step);
+
+    var e2e_tests = b.addTest("test/test.zig");
+    e2e_tests.setBuildMode(mode);
+    e2e_tests.addPackagePath("yaml", "src/yaml.zig");
+    test_step.dependOn(&e2e_tests.step);
 }
--- examples/yaml.zig
+++ examples/yaml.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const build_options = @import("build_options");
 const yaml = @import("yaml");
 
 const io = std.io;
@@ -10,32 +11,87 @@ const usage =
     \\Usage: yaml <path-to-yaml>
     \\
     \\General options:
-    \\-h, --help    Print this help and exit
+    \\--debug-log [scope]           Turn on debugging logs for [scope] (requires program compiled with -Dlog)
+    \\-h, --help                    Print this help and exit
+    \\
 ;
+
+var log_scopes: std.ArrayList([]const u8) = std.ArrayList([]const u8).init(gpa);
+
+pub fn log(
+    comptime level: std.log.Level,
+    comptime scope: @TypeOf(.EnumLiteral),
+    comptime format: []const u8,
+    args: anytype,
+) void {
+    // Hide debug messages unless:
+    // * logging enabled with `-Dlog`.
+    // * the --debug-log arg for the scope has been provided
+    if (@enumToInt(level) > @enumToInt(std.log.level) or
+        @enumToInt(level) > @enumToInt(std.log.Level.info))
+    {
+        if (!build_options.enable_logging) return;
+
+        const scope_name = @tagName(scope);
+        for (log_scopes.items) |log_scope| {
+            if (mem.eql(u8, log_scope, scope_name)) break;
+        } else return;
+    }
+
+    // We only recognize 4 log levels in this application.
+    const level_txt = switch (level) {
+        .err => "error",
+        .warn => "warning",
+        .info => "info",
+        .debug => "debug",
+    };
+    const prefix1 = level_txt;
+    const prefix2 = if (scope == .default) ": " else "(" ++ @tagName(scope) ++ "): ";
+
+    // Print the message to stderr, silently ignoring any errors
+    std.debug.print(prefix1 ++ prefix2 ++ format ++ "\n", args);
+}
 
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(gpa.allocator());
     defer arena.deinit();
     const allocator = arena.allocator();
 
-    const args = try std.process.argsAlloc(allocator);
-    if (args.len == 1) {
-        try io.getStdErr().writeAll("fatal: no input path to yaml file specified");
-        try io.getStdOut().writeAll(usage);
-        return;
+    const all_args = try std.process.argsAlloc(allocator);
+    const args = all_args[1..];
+
+    const stdout = io.getStdOut().writer();
+    const stderr = io.getStdErr().writer();
+
+    var file_path: ?[]const u8 = null;
+    var arg_index: usize = 0;
+    while (arg_index < args.len) : (arg_index += 1) {
+        if (mem.eql(u8, "-h", args[arg_index]) or mem.eql(u8, "--help", args[arg_index])) {
+            return io.getStdOut().writeAll(usage);
+        } else if (mem.eql(u8, "--debug-log", args[arg_index])) {
+            if (arg_index + 1 >= args.len) {
+                return stderr.writeAll("fatal: expected [scope] after --debug-log\n\n");
+            }
+            arg_index += 1;
+            if (!build_options.enable_logging) {
+                try stderr.writeAll("warn: --debug-log will have no effect as program was not built with -Dlog\n\n");
+            } else {
+                try log_scopes.append(args[arg_index]);
+            }
+        } else {
+            file_path = args[arg_index];
+        }
     }
 
-    if (mem.eql(u8, "-h", args[1]) or mem.eql(u8, "--help", args[1])) {
-        try io.getStdOut().writeAll(usage);
-        return;
-    } else {
-        const file_path = args[1];
-        const file = try std.fs.cwd().openFile(file_path, .{});
-        defer file.close();
-
-        const source = try file.readToEndAlloc(allocator, std.math.maxInt(u32));
-
-        var parsed = try yaml.Yaml.load(allocator, source);
-        try parsed.stringify(io.getStdOut().writer());
+    if (file_path == null) {
+        return stderr.writeAll("fatal: no input path to yaml file specified\n\n");
     }
+
+    const file = try std.fs.cwd().openFile(file_path.?, .{});
+    defer file.close();
+
+    const source = try file.readToEndAlloc(allocator, std.math.maxInt(u32));
+
+    var parsed = try yaml.Yaml.load(allocator, source);
+    try parsed.stringify(stdout);
 }
