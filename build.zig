c530ffbc0a3e1cf045044c3a38738eee6c0c90b5
sencrash <admin@xdb.be>
2024-06-07 20:24:50 +0200

    yaml-testsuite: add run step to generated spec-tests

--- build.zig
+++ build.zig
@@ -53,6 +53,12 @@ pub fn build(b: *std.Build) void {
     const enable_spec_tests = b.option(bool, "enable-spec-tests", "Enable YAML Test Suite") orelse false;
     if (enable_spec_tests) {
         const gen = SpecTest.create(b);
-        test_step.dependOn(&gen.step);
+        var spec_tests = b.addTest(.{
+            .root_source_file = gen.path(),
+            .target = target,
+            .optimize = optimize,
+        });
+        spec_tests.root_module.addImport("yaml", yaml_module);
+        test_step.dependOn(&b.addRunArtifact(spec_tests).step);
     }
 }
