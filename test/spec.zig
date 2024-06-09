14bce6671c0efb717ac69c5d8fe4b311fb08db9d
sencrash <admin@xdb.be>
2024-06-09 05:51:33 +0200

    yaml-testsuite: full prototype

--- test/spec.zig
+++ test/spec.zig
@@ -23,7 +23,7 @@ const preamble =
     \\const alloc = testing.allocator;
     \\
     \\fn loadFromFile(file_path: []const u8) !Yaml {
-    \\    const file = try std.fs.cwd().openFile(file_path, .{});
+    \\    const file = try std.fs.openFileAbsolute(file_path, .{});
     \\    defer file.close();
     \\
     \\    const source = try file.readToEndAlloc(alloc, std.math.maxInt(u32));
@@ -32,6 +32,13 @@ const preamble =
     \\    return Yaml.load(alloc, source);
     \\}
     \\
+    \\fn loadFileString(file_path: []const u8) ![]u8 {
+    \\    const file = try std.fs.openFileAbsolute(file_path, .{});
+    \\    defer file.close();
+    \\
+    \\    const source = try file.readToEndAlloc(alloc, std.math.maxInt(u32));
+    \\    return source;
+    \\}
     \\
 ;
 
@@ -98,7 +105,15 @@ fn make(step: *Step, prog_node: std.Progress.Node) !void {
     try writer.writeAll(preamble);
 
     while (testcases.popOrNull()) |kv| {
+        b.allocator.free(kv.key);
         try emitTest(b.allocator, &output, kv.value);
+        b.allocator.free(kv.value.name);
+        var tags = kv.value.tags;
+        tags.deinit();
+        b.allocator.free(kv.value.path);
+        if (kv.value.result == .expected_output_path) {
+            b.allocator.free(kv.value.result.expected_output_path);
+        }
     }
 
     const key = mem.zeroes([Aegis128LMac_128.key_length]u8);
@@ -190,10 +205,58 @@ fn collectTest(alloc: mem.Allocator, entry: fs.Dir.Walker.Entry, testcases: *std
     }
 }
 
+const no_output_template =
+    \\    var yaml = try loadFromFile("{s}");
+    \\    defer yaml.deinit();
+    \\
+;
+
+const expect_file_template =
+    \\    var yaml = try loadFromFile("{s}");
+    \\    defer yaml.deinit();
+    \\
+    \\    const expected = try loadFileString("{s}");
+    \\    defer alloc.free(expected);
+    \\
+    \\    var buf = std.ArrayList(u8).init(alloc);
+    \\    defer buf.deinit();
+    \\    try yaml.stringify(&buf.writer());
+    \\    const actual = try buf.toOwnedSlice();
+    \\    try testing.expect(std.meta.eql(expected, actual));
+    \\
+;
+
+const expect_err_template =
+    \\    var yaml = loadFromFile("{s}") catch return;
+    \\    defer yaml.deinit();
+    \\    return error.UnexpectedSuccess;
+    \\
+;
+
 fn emitTest(alloc: mem.Allocator, output: *std.ArrayList(u8), testcase: Testcase) !void {
-    const data = std.fmt.allocPrint(alloc, "test \"{}\" {{\n    var yaml = try loadFromFile(\"{s}\");\n    defer yaml.deinit();\n}}\n\n", .{ std.zig.fmtEscapes(testcase.name), testcase.path }) catch @panic("OOM");
-    defer alloc.free(data);
-    try output.appendSlice(data);
+    const head = std.fmt.allocPrint(alloc, "test \"{}\" {{\n", .{std.zig.fmtEscapes(testcase.name)}) catch @panic("OOM");
+    defer alloc.free(head);
+    try output.appendSlice(head);
+
+    switch (testcase.result) {
+        .none => {
+            const body = std.fmt.allocPrint(alloc, no_output_template, .{testcase.path}) catch @panic("OOM");
+            defer alloc.free(body);
+            try output.appendSlice(body);
+        },
+        .expected_output_path => {
+            const body = std.fmt.allocPrint(alloc, expect_file_template, .{ testcase.path, testcase.result.expected_output_path }) catch @panic("OOM");
+            defer alloc.free(body);
+            try output.appendSlice(body);
+        },
+        .error_expected => {
+            const body = std.fmt.allocPrint(alloc, expect_err_template, .{testcase.path}) catch @panic("OOM");
+            defer alloc.free(body);
+            try output.appendSlice(body);
+        },
+    }
+
+    try output.appendSlice("}\n\n");
 }
 
 fn canAccess(dir: fs.Dir, file_path: []const u8) bool {
