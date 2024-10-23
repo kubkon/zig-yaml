2cd0fc2e07f1ec491538014cc2c16caaec5e8ef0
caleberi <caleberioluwa@gmail.com>
2024-10-23 23:59:02 +0100

    feat: test support for yaml 1.1 and 1.2 backward compatiblity

--- test/test.zig
+++ test/test.zig
@@ -28,6 +28,7 @@ test "simple" {
         },
         finally: [4]f16,
         isyaml: bool,
+        hasBoolean: bool,
 
         pub fn eql(self: @This(), other: @This()) bool {
             if (self.names.len != other.names.len) return false;
@@ -63,9 +64,10 @@ test "simple" {
         .nested = .{
             .some = "one",
             .wick = "john doe",
-            .ok = false,
+            .ok = true,
         },
         .isyaml = false,
+        .hasBoolean = false,
         .finally = [_]f16{ 8.17, 19.78, 17, 21 },
     };
     try testing.expect(result.eql(expected));
