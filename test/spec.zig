ec4db99b5b60e962eec810ce2d6e21097da28a75
Andrey Ovchinnikov <s21v_ovchinnikov@179.ru>
2025-01-26 03:51:30 +0300

    fix error during 'zig build'

--- test/spec.zig
+++ test/spec.zig
@@ -69,9 +69,7 @@ const Testcase = struct {
     tags: std.BufSet,
 };
 
-fn make(step: *Step, make_options: Step.MakeOptions) !void {
-    _ = make_options;
-
+fn make(step: *Step, _: std.Progress.Node) !void {
     const spec_test: *SpecTest = @fieldParentPtr("step", step);
     const b = step.owner;
 
