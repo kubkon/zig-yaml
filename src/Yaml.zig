8334ed399e52a809ff8fc499a383c047b60b1070
Andrew Hart <andrew@achart.me.uk>
2025-04-18 22:30:34 +0100

    Support enum types

--- src/Yaml.zig
+++ src/Yaml.zig
@@ -96,6 +96,7 @@ fn parseValue(self: Yaml, arena: Allocator, comptime T: type, value: Value) Erro
         .float => self.parseFloat(T, value),
         .@"struct" => self.parseStruct(arena, T, try value.asMap()),
         .@"union" => self.parseUnion(arena, T, value),
+        .@"enum" => std.meta.stringToEnum(T, try value.asScalar()) orelse Error.EnumTagMissing,
         .array => self.parseArray(arena, T, try value.asList()),
         .pointer => if (value.asList()) |list| {
             return self.parsePointer(arena, T, .{ .list = list });
@@ -262,6 +263,7 @@ pub const Error = error{
     Unimplemented,
     TypeMismatch,
     StructFieldMissing,
+    EnumTagMissing,
     ArraySizeMismatch,
     UntaggedUnion,
     UnionTagMissing,
@@ -571,6 +573,8 @@ pub const Value = union(enum) {
                     }
                 } else unreachable;
             } else return error.UntaggedUnion,
+
+            .@"enum" => return try encode(arena, @tagName(input)),
 
             .array => return encode(arena, &input),
 
--- src/Yaml/test.zig
+++ src/Yaml/test.zig
@@ -179,6 +179,51 @@ test "simple flow sequence / bracket list with double trailing commas" {
     try std.testing.expectError(error.ParseFailure, err);
 }
 
+const TestEnum = enum {
+    alpha,
+    bravo,
+    charlie,
+};
+
+test "enums" {
+    const source =
+        \\- alpha
+        \\- bravo
+        \\- charlie
+    ;
+
+    var yaml: Yaml = .{ .source = source };
+    defer yaml.deinit(testing.allocator);
+    try yaml.load(testing.allocator);
+
+    try testing.expectEqual(yaml.docs.items.len, 1);
+
+    var arena = Arena.init(testing.allocator);
+    defer arena.deinit();
+
+    const arr = try yaml.parse(arena.allocator(), [3]TestEnum);
+    try testing.expectEqualSlices(TestEnum, &[_]TestEnum{ .alpha, .bravo, .charlie }, &arr);
+}
+
+test "invalid enum" {
+    const source =
+        \\- delta
+        \\- echo
+    ;
+
+    var yaml: Yaml = .{ .source = source };
+    defer yaml.deinit(testing.allocator);
+    try yaml.load(testing.allocator);
+
+    try testing.expectEqual(yaml.docs.items.len, 1);
+
+    var arena = Arena.init(testing.allocator);
+    defer arena.deinit();
+
+    const result = yaml.parse(arena.allocator(), [2]TestEnum);
+    try testing.expectError(Yaml.Error.EnumTagMissing, result);
+}
+
 test "simple map untyped" {
     const source =
         \\a: 0
@@ -781,4 +826,10 @@ test "struct default value test" {
         try testing.expectEqual(tc.container.c, parsed.c);
         try testing.expectEqual(tc.container.d, parsed.d);
     }
+}
+
+test "stringify an enum" {
+    try testStringify("alpha", TestEnum.alpha);
+    try testStringify("bravo", TestEnum.bravo);
+    try testStringify("charlie", TestEnum.charlie);
 }
